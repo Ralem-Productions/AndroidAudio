76af2355a2d7c2f9410bc8afed788b3c8d7e6d63
ralem1234 <ralem1234@yahoo.com>
2015-07-09 01:08:54 -0500

    Recording and Playback working

--- SoundChanger/app/src/main/java/com/ralemproductions/soundchanger/MainActivity.java
+++ SoundChanger/app/src/main/java/com/ralemproductions/soundchanger/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ralemproductions.soundchanger;
 
+import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Debug;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,60 +19,66 @@ import java.io.File;
 import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    private MediaRecorder recorder = null;
+    public MediaRecorder recorder = null;
+    private MediaPlayer player;
+    private String OUTPUT_FILE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OUTPUT_FILE = Environment.getExternalStorageDirectory() + "/audiorecorder.3gpp";
     }
 
+    public void StartClick(View view) throws IOException {
+        if (recorder != null) {
+            recorder.release();
+        }
 
-    public void StartClick(View view) {
+        File outFile = new File(OUTPUT_FILE);
+
+        if (outFile.exists()) {
+            outFile.delete();
+        }
+
         recorder = new MediaRecorder();
-
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        recorder.setOutputFile(getFilePath());
-
+        recorder.setOutputFile(OUTPUT_FILE);
         try {
             recorder.prepare();
-            recorder.start();
-        }catch(Exception e){
-
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
         }
+        recorder.start();
     }
 
-    private String getFilePath()
-    {
-        String filePath = Environment.getExternalStorageDirectory().getPath();
-        File file = new File(filePath, "MediaRecorderSample");
-
-        if(!file.exists()) {
-            file.mkdir();
-        }
-
-        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".mp4");
-    }
 
     public void StopClick(View view)
     {
         if(recorder != null) {
-            recorder.stop();
-            recorder.reset();
-            recorder.release();
-
-
-
-            recorder = null;
+                recorder.stop();
         }
     }
 
     public void PlayBackClick(View view)
     {
-
+        if(player != null){player.release();}
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(OUTPUT_FILE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            player.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        player.start();
     }
 }
